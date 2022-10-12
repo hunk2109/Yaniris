841e9e3c1b97c33cce32b1e47d9cf01ad71f9c59
Hector Jose Taveras Rodriguez <hectojo_2109@hotmail.com>
2022-10-12 16:22:43 -0400

    REMOCION DE ARCHIVOS INECESARIOS

--- sistgre/Form1.Designer.cs
+++ sistgre/Form1.Designer.cs
@@ -3320,6 +3320,7 @@
             this.ClientSize = new System.Drawing.Size(1370, 749);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sierra";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabPage5.ResumeLayout(false);
